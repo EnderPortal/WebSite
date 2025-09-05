e8e3307ff1b108ab4491433333db9106367fec1c
NKRI <nkri.dev@gmail.com>
2025-09-05 18:33:47 +0200

    create a utils class to manage access token

---
+++ src/utils/auth.ts
@@ -0,0 +1,32 @@
+/**
+ * Puts the access token in localstorage
+ * @param access_token token
+ */
+export function setToken(access_token: string){
+    localStorage.setItem("token", access_token);
+}
+
+/**
+ * retrieve the access token from local storage
+ * @returns access_token => string format
+ */
+export function getToken(){
+    return localStorage.getItem("token");
+}
+
+/**
+ * check if the user is authenticated 
+ * (access token in local storage)
+ * 
+ * @returns True if is authenticated
+ */
+export function isAuthenticated(){
+    return !!getToken();
+}
+
+/**
+ * Delete access token from localstorage
+ */
+export function removeToken(){
+    localStorage.removeItem("token");
+}
