a3d8ade9bf9c5fd82493a6b6eafe4ebfd03f5c59
NKRI <nkri.dev@gmail.com>
2025-10-03 20:06:05 +0200

    fix(Home.tsx) : Handling user object nullity

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,5 +3,17 @@ import { useUser } from "../hooks/useUser";
 export default function Home() {
   const {user} = useUser();
 
-  return <h1>Salut ! {user?.username}</h1>
+  return (
+    <div className="text-white">
+      {
+        user ? (
+          <h1>Bonjour ! {user.username}</h1>
+        ) 
+        :
+        (
+          <h1>Chargement...</h1>
+        )
+      }
+    </div>
+  )
 }
